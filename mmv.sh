a5270df4d4e78e8c95a6244cfc9341434c693c96
McAuley Penney <134115@gmail.com>
2021-12-04 18:11:00 -0700

    docs: update comments and function documentation

--- mmv.sh
+++ mmv.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
+################################################################################
+# mmv main
+#
+# Globals:
+#     - new_name_dict: associative array for holding key
+#                      val pairs relating current names to new names
+# Arguments:
+#     - $@: all files and dirs to change names of
+# Outputs:
+#     - changes given files to new file names provided by user
+# Returns:
+#     - none
+################################################################################
 mmv()
 {
     if test $# -gt 0
@@ -18,17 +31,18 @@ mmv()
 
 
 
-# Does:
-#   1. creates temp file to edit in
-#      Note: I avoid /tmp, though I'd
-#            prefer it, so that I can
-#            avoid sudo during
-#            deletion at the end
-#   2. opens file
-#   3. cleans input
-#   4. collects user input from file
-#      into array
-#   5. deletes temp file
+################################################################################
+# Gets new name input from user via $EDITOR
+#
+# Globals:
+#     - none
+# Arguments:
+#     $@: all files to modify
+# Outputs:
+#     - out_names_arr: see create_name_dict()
+# Returns:
+#     - none
+################################################################################
 get_input()
 {
     # create temp file to write in
@@ -38,78 +52,105 @@ get_input()
     # echo all arg names into temp file
     printf "%s\n" "$@" > "$temp_file"
 
+    # open temp file in editor
     $EDITOR "$temp_file"
 
+    # sanitize user input
     clean_input "$temp_file"
 
+    # populate global associative array
+    # containing key val pairs linking old
+    # and new names
     create_name_dict "$temp_file" "$@"
 
-    # delete temp editing file
+    # delete temp file which was used
+    # to gather new name args
     rm -f "$temp_file "1> /dev/null
 }
 
 
 
 
-# Does:
-#   1. expands any tildes into
-#      the absolute path of $HOME
-#   2. removes empty lines
+
+################################################################################
+# sanitize user input given in text file
+#
+# Globals:
+#     - none
+# Arguments:
+#     $1: in_file; file containing new file
+#         names. Created by user in get_input()
+# Outputs:
+#     - sanitized user input in in_file
+# Returns:
+#     - none
+################################################################################
 clean_input()
 {
     local in_file=$1
     local temp_file
     temp_file=$(mktemp ~/.cache/mmv-XXXXX)
 
-    # expand tildes
+    # expand any tildes in new names
     sed -i "s.~.${HOME}." "$in_file"
 
-    # remove all newline chars
+    # remove all newline chars so that
+    # all vals, such as old names and
+    # new names "line up" by index and
+    # write to temp file
     "$(awk NF "$in_file")" > "$temp_file"
 
-    # write to arg file
+    # write temp file to out parameter
     mv "$temp_file" "$in_file"
 }
 
 
 
 
-# Does:
-#   1. creates array from user input
-#      in file
-#   2. loops through old names given
-#      as CLI args
-#       a. if old name does not match
-#          the new name given as input
-#           i. expand the path of the old
-#              name
-#           ii. assign old name as key, new
-#               name as val in dict
+################################################################################
+# populates associative array, linking old
+# names and new names
+#
+# Globals:
+#     - creates none
+#     - populates new_name_dict, init in mmv()
+# Arguments:
+#     $1: in_file; file containing new file
+#         names. Created by user in get_input()
+# Outputs:
+#     - populated associative array
+# Returns:
+#     - none
+################################################################################
 create_name_dict()
 {
     local i=0
     local -a out_names_arr
     local in_file=$1
 
-    # read contents into arr
+    # read user-chosen new names into arr
     readarray -t out_names_arr < "$in_file"
 
     # loop through original names in arg list
     for old_name in "${@:2}"
     do
-        # expand old name to abs path
+        # get current destination name/new name
         local dest=${out_names_arr[$i]}
 
-        # compare old to new to determine
-        # if we want to keep
+        # if the old name does not match the
+        # new name (meaning that the user
+        # actually changed the name instead
+        # of leaving it as is)
         if [[ $old_name != "$dest" ]]
         then
 
+            # expand the path of the old name
             local expand_old
             expand_old=$(readlink -f "$old_name")
 
             # create associative array where
-            # key = old name, val = new name
+            # key = expanded old name and
+            # val = new name
             new_name_dict[$expand_old]=$dest
         fi
 
@@ -120,29 +161,30 @@ create_name_dict()
 
 
 
-conduct_mvs()
-{
-    for old_name in "${!new_name_dict[@]}"
-    do
-        local dest=${new_name_dict[${old_name}]}
-
-        if [[ -n $dest ]]
-        then
-            mkdir -p "$(dirname "${dest}")"; mv "$old_name" "$dest"
-        fi
-    done
-}
-
-
-
-
+################################################################################
+# loop through all entries in associative array of
+# names and conduct any renames necessary because of
+# collisions, i.e. new chosen names already exist in fs
+#
+# Globals:
+#     - creates none
+# Arguments:
+#     - none
+# Outputs:
+#     - rename all files that will result in collision
+#       with name provided as destination by user
+# Returns:
+#     - none
+################################################################################
 conduct_renames()
 {
-    # change all names that conflict
+    # for all keys (old names) in global name dict
     for old_name in "${!new_name_dict[@]}"
     do
+        # get associated val
         local dest=${new_name_dict[${old_name}]}
 
+        # if destination is a directory
         if test -d "$dest"
         then
 
@@ -157,9 +199,10 @@ conduct_renames()
             # nullify in dict
             new_name_dict[$old_name]=""
 
-        # if file already exists rename file with dest name to temp name
+        # if file already exists with destination name
         elif test -f "$dest"
         then
+            # rename file with dest name to temp name
             rename_file "$dest"
         fi
     done
@@ -168,9 +211,25 @@ conduct_renames()
 
 
 
+################################################################################
+# rename individual item in fs to a temp name
+# based on original name
+#
+# Globals:
+#     - creates none
+#     - uses new_name_dict
+# Arguments:
+#     $1: name_to_mv; a file in currently in fs
+#         whose name is desired by a file to be
+#         renamed by mmv
+# Outputs:
+#     - rename $1
+# Returns:
+#     - none
+################################################################################
 rename_file()
 {
-    # alias args
+    # get full path of $1
     local name_to_mv
     name_to_mv=$(readlink -f "$1")
 
@@ -178,11 +237,19 @@ rename_file()
     local tmp_name
     tmp_name=$(mktemp -u mmv_XXXXX_"$(basename "$name_to_mv")")
 
+    # mv arg item to temp name
     mv "$name_to_mv" "$tmp_name"
 
+    # get destination, if it exists, of item in fs.
+    # this will only produce a non-zero len str if
+    # the item that we are renaming is also in the
+    # arr of items to be moved. If the item that was
+    # renamed is NOT in the array, it was simply an
+    # item in the fs that had the same name as one that
+    # the user provided to mmv as a destination
     local mv_name_dest=${new_name_dict[${name_to_mv}]}
 
-    # if destination exists
+    # if destination is a non-zero len str
     if [[ -n $mv_name_dest ]]
     then
         # nullify old entry
@@ -192,4 +259,42 @@ rename_file()
         new_name_dict+=( [$tmp_name]=$mv_name_dest )
     fi
 
+}
+
+
+
+
+################################################################################
+# loop through all entries in associative array
+# and rename original items to their destination
+# names
+#
+# Globals:
+#     - creates none
+#     - uses new_name_dict
+# Arguments:
+#     - none
+# Outputs:
+#     - rename all files to destination names
+# Returns:
+#     - none
+################################################################################
+conduct_mvs()
+{
+    # for all keys (old names) in global name dict
+    for old_name in "${!new_name_dict[@]}"
+    do
+        # get associated val
+        local dest=${new_name_dict[${old_name}]}
+
+        # if the destination is a non-zero len str
+        # (meaning that that the item has not been
+        # deleted or renamed to a temp file in
+        # rename_file())
+        if [[ -n $dest ]]
+        then
+            # create any parent dirs and mv file to its destination
+            mkdir -p "$(dirname "${dest}")"; mv "$old_name" "$dest"
+        fi
+    done
 }
